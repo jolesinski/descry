d252a88cea7cc9f2f8d58a9f930e81133d5ccd6b
jolesinski <olesinki.jakub@gmail.com>
2017-05-05 09:17:05 +0000

    perf: dectectAndCompute vs separate detect and compute

--- test/perf/keypoints.cpp
+++ test/perf/keypoints.cpp
@@ -5,6 +5,9 @@
 #include <descry/keypoints.h>
 #include <descry/willow.h>
 
+#include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+
 static void BM_Keypoints_Time(benchmark::State& state, descry::Config&& cfg) {
     //TODO: load single model, and single test scene with ground truth
     // calculate model views and scene keypoints, extract gt keys from scene keys
@@ -42,3 +45,44 @@ static void BM_Keypoints_Time(benchmark::State& state, descry::Config&& cfg) {
 //BENCHMARK_CAPTURE(BM_Keypoints_Time, uniform, descry::test::keypoints::loadConfigUniform())->Unit(benchmark::kMillisecond);
 //BENCHMARK_CAPTURE(BM_Keypoints, iss, descry::test::keypoints::loadConfigISS())->Unit(benchmark::kMillisecond);
 BENCHMARK_CAPTURE(BM_Keypoints_Time, iss_cupcl, descry::test::keypoints::loadConfigCupcl())->Unit(benchmark::kMillisecond);
+
+cv::Mat loadSceneFrame() {
+    auto image = descry::Image(descry::test::loadSceneCloud());
+    const auto& full = image.getFullCloud().host();
+
+    //copy to opencv mat
+    typedef cv::Point3_<uint8_t> Pixel;
+    cv::Mat frame = cv::Mat::zeros(full->width, full->height, CV_8UC3);
+    frame.forEach<Pixel>([&](Pixel& pixel, const int position[]) -> void {
+        const auto& point = full->at(full->width - position[0] - 1, position[1]);
+        pixel.x = point.b;
+        pixel.y = point.g;
+        pixel.z = point.r;
+    });
+
+    return frame;
+}
+
+static void BM_OpenCV_ORB_detectAndCompute(benchmark::State& state) {
+    auto frame = loadSceneFrame();
+    std::vector<cv::KeyPoint> kp;
+    cv::Ptr<cv::ORB> orb = cv::ORB::create();
+    orb->setMaxFeatures(1000);
+    cv::Mat desc;
+    while (state.KeepRunning())
+        orb->detectAndCompute(frame, cv::noArray(), kp, desc);
+}
+BENCHMARK(BM_OpenCV_ORB_detectAndCompute);
+
+static void BM_OpenCV_ORB_detect_then_compute(benchmark::State& state) {
+    auto frame = loadSceneFrame();
+    std::vector<cv::KeyPoint> kp;
+    cv::Ptr<cv::ORB> orb = cv::ORB::create();
+    orb->setMaxFeatures(1000);
+    cv::Mat desc;
+    while (state.KeepRunning()) {
+        orb->detect(frame, kp);
+        orb->compute(frame, kp, desc);
+    }
+}
+BENCHMARK(BM_OpenCV_ORB_detect_then_compute);
