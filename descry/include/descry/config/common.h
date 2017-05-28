b9f1668c88a38a91af280ef21fe85c2baf9f6f99
jolesinski <olesinki.jakub@gmail.com>
2017-05-28 13:49:58 +0000

    config: common header

---
+++ descry/include/descry/config/common.h
@@ -0,0 +1,10 @@
+#ifndef DESCRY_CONFIG_COMMON_H
+#define DESCRY_CONFIG_COMMON_H
+
+namespace descry { namespace config {
+
+static constexpr auto TYPE_NODE = "type";
+
+}}
+
+#endif //DESCRY_CONFIG_COMMON_H
