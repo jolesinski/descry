74f2ea6ac7f4d932e5ab943dba2f34754e49fcfd
jolesinski <olesinki.jakub@gmail.com>
2017-06-09 20:48:40 +0000

    logger: added init and get

---
+++ descry/include/descry/logger.h
@@ -0,0 +1,22 @@
+#ifndef DESCRY_LOGGER_H
+#define DESCRY_LOGGER_H
+
+#include <spdlog/spdlog.h>
+
+namespace descry { namespace logger {
+
+static constexpr auto LOGGER_NAME = "descry";
+
+// TODO: configure
+void init() {
+    spdlog::set_async_mode(8192);
+    spdlog::stdout_color_mt(LOGGER_NAME);
+}
+
+auto get() {
+    return spdlog::get(LOGGER_NAME);
+}
+
+}}
+
+#endif //DESCRY_LOGGER_H
